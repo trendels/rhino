bada572f7bdb9eecfed1da02e91d6adaa2dd2025
Stanis Trendelenburg <stanis.trendelenburg@gmail.com>
2014-12-17 16:27:54 +0100

    Added resources for serving static files

--- rhino/__init__.py
+++ rhino/__init__.py
@@ -4,5 +4,6 @@ from .mapper import Mapper
 from .resource import Resource, get, post, put, delete, patch, options
 from .request import Request
 from .response import Response, Entity, ok, redirect
+from .static import StaticFile, StaticDirectory
 
 __version__ = '0.0.1'
