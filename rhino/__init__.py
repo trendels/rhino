4aec9b35c0653ac209ce777f1a9f476a2012835f
Stanis Trendelenburg <stanis.trendelenburg@gmail.com>
2014-12-29 21:26:28 +0100

    Export cache_control from the rhino module

--- rhino/__init__.py
+++ rhino/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
+from .http import cache_control
 from .mapper import Mapper
-from .resource import Resource, get, post, put, delete, patch, options
 from .request import Request
+from .resource import Resource, get, post, put, delete, patch, options
 from .response import Response, Entity, ok, created, no_content, redirect
 from .static import StaticFile, StaticDirectory
 
