514986f9dcf14b031fece6eb1512acc2c599aafd
Stanis Trendelenburg <stanis.trendelenburg@gmail.com>
2014-12-30 17:59:05 +0100

    Preparations for packaging

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 
 version = '0.0.1'
 
+with open('README.rst') as f:
+    README = f.read()
+
 setup(
     name='Rhino',
     version=version,
@@ -12,4 +15,12 @@ setup(
     url='https://github.com/trendels/rhino',
     license='MIT',
     description='A microframework for building RESTful web services',
+    long_description=README,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
