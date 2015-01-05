de7b33a32b94526a041740374454bce5d6a7028d
Stanis Trendelenburg <stanis.trendelenburg@gmail.com>
2015-01-05 23:13:32 +0100

    Pass headers as keyword args to test client

--- test/examples/test_conneg.py
+++ test/examples/test_conneg.py
@@ -6,14 +6,14 @@ client = TestClient(app.wsgi)
 
 
 def test_get_html():
-    res = client.get('/', {'HTTP_ACCEPT': 'text/html'})
+    res = client.get('/', accept='text/html')
     assert res.code == 200
     assert res.headers['Content-Type'] == 'text/html'
     assert res.body == '<html><h1>Hello, world!</h1></html>'
 
 
 def test_get_json():
-    res = client.get('/', {'HTTP_ACCEPT': 'application/json'})
+    res = client.get('/', accept='application/json')
     assert res.code == 200
     assert res.headers['Content-Type'] == 'application/json'
     assert res.body == '{"greeting": "Hello, world!"}'
@@ -22,5 +22,5 @@ def test_put_json():
     res = client.put('/', '{"greeting": "Howdy!"}', content_type='application/json')
     assert res.code == 200
 
-    res = client.get('/', {'HTTP_ACCEPT': 'application/json'})
+    res = client.get('/', accept='application/json')
     assert res.body == '{"greeting": "Howdy!"}'
--- test/examples/tutorial/test_service.py
+++ test/examples/tutorial/test_service.py
@@ -17,7 +17,7 @@ def test_todo_list(client):
     assert 'Profit!' in response.body
 
 def test_todo_list_json(client):
-    response = client.get('/todos', {'HTTP_ACCEPT': 'application/json'})
+    response = client.get('/todos', accept='application/json')
     assert response.code == 200
     assert response.headers.get('Content-Type') == 'application/json'
     todo_list = json.loads(response.body)
@@ -25,11 +25,11 @@ def test_todo_list_json(client):
     assert todo_list[-1]['href'] == 'http://localhost/todos/3'
 
 def test_todo_list_etag(client):
-    response = client.get('/todos', {'HTTP_ACCEPT': 'application/json'})
+    response = client.get('/todos', accept='application/json')
     assert 'ETag' in response.headers
     etag = response.headers['ETag']
-    response = client.get('/todos', {'HTTP_ACCEPT': 'application/json',
-                                     'HTTP_IF_NONE_MATCH': etag})
+    response = client.get(
+            '/todos', accept='application/json', if_none_match=etag)
     assert response.code == 304
     assert response.headers.get('ETag') == etag
     assert not response.body
